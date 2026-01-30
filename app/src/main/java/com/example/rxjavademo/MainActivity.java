59aefcf5224cc9d1ce05c405410878b045ede9d3
chenyang <chenyang@sunniwell.net>
2026-01-30 16:57:31 +0800

    Flowable的发射和订阅

--- app/src/main/java/com/example/rxjavademo/MainActivity.java
+++ app/src/main/java/com/example/rxjavademo/MainActivity.java
@@ -2,21 +2,16 @@ package com.example.rxjavademo;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
-
 import java.util.concurrent.TimeUnit;
-
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.ObservableEmitter;
-import io.reactivex.rxjava3.core.ObservableOnSubscribe;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
@@ -24,7 +19,7 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private TextView observerTextView;
-    private Button mObserverBtn;
+    private Button mObserverBtn,mStartBtn,mStopBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         observerTextView = findViewById(R.id.observerTextView);
         mObserverBtn = findViewById(R.id.observerBtn);
+        mStartBtn = findViewById(R.id.StartBtn);
+        mStopBtn = findViewById(R.id.StopBtn);
 
         Observer observer = new Observer<String>() {//观察者
             @Override
@@ -64,8 +61,9 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
-        Observable<String> observable = Observable
+        Observable<String> observable = Observable//被观察者
                 .just("hello", "RxJava")
+                .startWithItem("我要开始喽")
                 .delay(2, TimeUnit.SECONDS)      // 模拟耗时
                 .subscribeOn(Schedulers.io())   // 子线程执行
                 .observeOn(AndroidSchedulers.mainThread()); // 回主线程
@@ -75,31 +73,49 @@ public class MainActivity extends AppCompatActivity {
             observable.subscribe(observer);//被观察者订阅观察者
         });
 
+        // 保存当前 Subscription
+        final Subscription[] currentSub = new Subscription[1];//让停止按钮可以拿到当前在工作的Subscription
+        mStartBtn.setOnClickListener(v -> {
+            Flowable.range(0,10)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Subscriber<Integer>() {//同步写法
+                        Subscription sub;
+                        //当订阅后，会首先调用这个方法，其实就相当于onStart()，
+                        //传入的Subscription s参数可以用于请求数据或者取消订阅
+                        @Override
+                        public void onSubscribe(Subscription s) {
+                            Log.w("TAG","onsubscribe start");
+                            sub=s;
+                            currentSub[0] = s;
+                            sub.request(1);//告诉上游：我现在最多还能处理1个数据   cancel()取消申请数据，断流，之后在request（）无法申请到数据
+                            Log.w("TAG","onsubscribe end");
+                        }
 
+                        @Override
+                        public void onNext(Integer o) {
+                            Log.w("TAG","onNext--->"+o+" thread=" + Thread.currentThread().getName());
+                            currentSub[0].request(1);
+                        }
+                        @Override
+                        public void onError(Throwable t) {
+                            t.printStackTrace();
+                        }
+                        @Override
+                        public void onComplete() {
+                            Log.w("TAG","onComplete");
+                            currentSub[0] = null;
+                        }
+                    });
+        });
 
-
-
-
-        Subscriber subscriber = new Subscriber<String>() {
-            @Override
-            public void onSubscribe(Subscription s) {//新的onStart，会在事件还未被发送之前调用，做一些准备工作，Disposable用于取消操作
-
+        mStopBtn.setOnClickListener(v -> {
+            if (currentSub[0] != null) {
+                currentSub[0].cancel();
+                Log.w("TAG","cancel called");
+                currentSub[0] = null;
             }
+        });
 
-            @Override
-            public void onNext(String o) {
-
-            }
-
-            @Override
-            public void onError(Throwable t) {
-                Log.d(TAG,"Error");
-            }
-
-            @Override
-            public void onComplete() {
-                Log.d(TAG,"Completed!");
-            }
-        };
     }
 }
